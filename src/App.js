ea531ac6175da7aeb165fff9f56db00fb1234ffd
blase147 <solarmails2@gmail.com>
2022-12-05 09:42:32 +0100

    Add basic files and linter setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from 'react-router-dom';
+import Header from './components/Header';
+import Myprofile from './components/Myprofile';
+import Rockets from './components/Rockets/Rockets';
+import Missions from './components/Missions/Missions';
+import Dragons from './components/Dragons/Dragons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/myProfile" element={<Myprofile />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/dragons" element={<Dragons />} />
+      </Routes>
+    </Router>
   );
 }
 
